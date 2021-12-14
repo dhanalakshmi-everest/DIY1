dea6971579320ae17e11e7293b91cb2959b0ad7d
Dhanalakshmi N <dhanalakshmi.narala@everest.engineering>
2021-12-14 11:00:09 +0530

    Add add products to store api

--- model.go
+++ model.go
@@ -8,7 +8,6 @@ import (
 	// "errors"
 )
 
-
 // tom: add backticks to json
 type product struct {
 	ID    int     `json:"id"`
@@ -73,6 +72,30 @@ func getProducts(db *sql.DB, start, count int) ([]product, error) {
 	rows, err := db.Query(
 		"SELECT id, name,  price FROM products LIMIT $1 OFFSET $2",
 		count, start)
+
+	if err != nil {
+		return nil, err
+	}
+
+	defer rows.Close()
+
+	products := []product{}
+
+	for rows.Next() {
+		var p product
+		if err := rows.Scan(&p.ID, &p.Name, &p.Price); err != nil {
+			return nil, err
+		}
+		products = append(products, p)
+	}
+
+	return products, nil
+}
+
+func getStoreProducts(db *sql.DB, storeId int) ([]product, error) {
+	rows, err := db.Query(
+		"SELECT id, name, price FROM products INNER JOIN store ON store.product_id=products.id WHERE store.store_id=$1;",
+		storeId)
 
 	if err != nil {
 		return nil, err
