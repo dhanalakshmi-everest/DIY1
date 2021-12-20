1fc16e3d36abc02ac881b36d09e1f92cdc8cbc00
Dhanalakshmi N <dhanalakshmi.narala@everest.engineering>
2021-12-20 09:21:11 +0530

    Add database creation file

---
+++ database.sql
@@ -0,0 +1,14 @@
+CREATE TABLE IF NOT EXISTS products
+(
+    id SERIAL,
+    name TEXT NOT NULL,
+    price NUMERIC(10,2) NOT NULL DEFAULT 0.00,
+    CONSTRAINT products_pkey PRIMARY KEY (id)
+);
+
+CREATE TABLE IF NOT EXISTS store(
+    store_id INT,
+    product_id INT,
+    is_avialable BOOLEAN,
+    FOREIGN KEY(product_id) REFERENCES products(id)
+);
